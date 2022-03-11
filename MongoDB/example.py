6592e162b6e96ae2f9951ea1eebf7fefe592ab9c
Joris Heemskerk <joris.heemskerk@student.hu.nl>
2022-03-11 11:24:30 +0100

    changed code conventions to pep 8

--- MongoDB/example.py
+++ MongoDB/example.py
@@ -4,5 +4,5 @@ def first_product(collection):
     """Return first instance of collection"""
     return collection.find_one()
 
-collection = ses.getCollection(ses.getDatabase(ses.getClient(), "Dataset"), "Products")
+collection = ses.get_collection(ses.get_database(ses.get_client(), "Dataset"), "Products")
 print(first_product(collection))
--- MongoDB/session.py
+++ MongoDB/session.py
@@ -1,14 +1,14 @@
 from pymongo import MongoClient
 
-def getClient(host: str="Localhost", port: int=27017):
+def get_client(host: str="Localhost", port: int=27017):
     """Get a client for given host and port
     Default: Localhost, 27017"""
     return MongoClient(host, port)
 
-def getDatabase(client: MongoClient, database_name: str):
+def get_database(client: MongoClient, database_name: str):
     """Get requested database instance for given client"""
     return client[database_name]
 
-def getCollection(database, collection_name: str):
+def get_collection(database, collection_name: str):
     """Get collection from given database"""
     return database[collection_name]
