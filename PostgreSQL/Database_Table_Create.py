a4a6c413a2edbfe43b89bd59c14d1cc6cfb0e17c
Joris Heemskerk <joris.heemskerk@student.hu.nl>
2022-03-11 14:37:12 +0100

    change collum to column

--- PostgreSQL/Database_Table_Create.py
+++ PostgreSQL/Database_Table_Create.py
@@ -4,25 +4,25 @@ import Connect_db
 con = Connect_db.connectdb('localhost','opisop_sql','password')
 
 
-def create_table(name, collum_strings):
-    sql_statement = f'CREATE TABLE {name} (' + ','.join(collum_strings) + ')'
+def create_table(name, column_strings):
+    sql_statement = f'CREATE TABLE {name} (' + ','.join(column_strings) + ')'
 
     return sql_statement
 
 
 def create_colum_string(name, type, etc):
-    collum_string = f'{name} {type} {etc}'
-    return collum_string
+    column_string = f'{name} {type} {etc}'
+    return column_string
 
 
-collums = [['id', 'VARCHAR(32)', 'NOT NULL'],
+columns = [['id', 'VARCHAR(32)', 'NOT NULL'],
            ['name', 'VARCHAR(256)', ''],
            ['price', 'INT', '']
            ]
-collum_list = []
-for i in collums:
-    collum_list.append(create_colum_string(i[0], i[1], i[2]))
-sql_statement = create_table('products', collum_list)
+column_list = []
+for i in columns:
+    column_list.append(create_colum_string(i[0], i[1], i[2]))
+sql_statement = create_table('products', column_list)
 cur = con.cursor()
 cur.execute(sql_statement)
 cur.close()
