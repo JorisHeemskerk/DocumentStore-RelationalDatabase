37564e592a71189440f22e264ea5da25aaab8af8
Joris Heemskerk <joris.heemskerk@student.hu.nl>
2022-03-11 14:40:35 +0100

    change postgres styling

--- PostgreSQL/Database_Table_Create.py
+++ PostgreSQL/Database_Table_Create.py
@@ -1,28 +1,28 @@
-import psycopg2
-import Connect_db
+import connect_db
 
-con = Connect_db.connectdb('localhost','opisop_sql','password')
-
+con = connect_db.connectdb('localhost','opisop_sql','password')
 
 def create_table(name, column_strings):
     sql_statement = f'CREATE TABLE {name} (' + ','.join(column_strings) + ')'
 
     return sql_statement
 
-
 def create_colum_string(name, type, etc):
     column_string = f'{name} {type} {etc}'
     return column_string
 
+columns = [
+    ['id', 'VARCHAR(32)', 'NOT NULL'],
+    ['name', 'VARCHAR(256)', ''],
+    ['price', 'INT', '']
+]
 
-columns = [['id', 'VARCHAR(32)', 'NOT NULL'],
-           ['name', 'VARCHAR(256)', ''],
-           ['price', 'INT', '']
-           ]
 column_list = []
 for i in columns:
     column_list.append(create_colum_string(i[0], i[1], i[2]))
+
 sql_statement = create_table('products', column_list)
+
 cur = con.cursor()
 cur.execute(sql_statement)
 cur.close()
