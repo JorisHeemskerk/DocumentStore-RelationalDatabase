763471e1c0592401005f17556271285fd15a9995
Boris de Graaf <b.z.degraaf@student.tudelft.nl>
2022-03-11 12:31:10 +0100

    connectie met PostGreSQL gefixt

--- PostgreSQL/Connect db.py
+++ PostgreSQL/Connect db.py
@@ -1,11 +1,20 @@
 import psycopg2
 
 
-def connectdb(host,database):
+def connectdb():
     con = psycopg2.connect(
-        host=host,
+        host='localhost',
+        database='test2',
         user='postgres',
-        database=database,
-        password='postgres',
+        password='uwu'
     )
     return con
+
+def insert(sql):
+    con = connectdb()
+    cur = con.cursor()
+    cur.execute(sql, ('a', 'b'))
+    con.commit()
+
+
+sql = 'INSERT INTO "testTable" (var1, var2) VALUES(%s, %s)'
