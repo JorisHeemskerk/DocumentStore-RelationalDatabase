8451d3d36b875f68133f5340cdf0cc01770f82a0
Joris Heemskerk <joris.heemskerk@student.hu.nl>
2022-03-11 14:32:33 +0100

    change function name to include all items, not just products

--- MongoDB/yield.py
+++ MongoDB/yield.py
@@ -1,32 +1,32 @@
 import session as ses
 
-def get_product_information(product, labels):
-    """Get array of infromation from given product, based on labels"""
-    prodcuct_info = []
+def get_item_information(item, labels):
+    """Get array of infromation from given item, based on labels"""
+    item_info = []
     for label in labels:
         if isinstance(label, list):
             if len(label) == 2:
                 try:
-                    prodcuct_info.append(product[label[0]][label[1]])
+                    item_info.append(item[label[0]][label[1]])
                 except KeyError:
-                    prodcuct_info.append(None)
+                    item_info.append(None)
             elif len(label) == 3:
                 try:
-                    prodcuct_info.append(product[label[0]][label[1]][label[2]])
+                    item_info.append(item[label[0]][label[1]][label[2]])
                 except KeyError:
-                    prodcuct_info.append(None)
+                    item_info.append(None)
         else:
             try:
-                prodcuct_info.append(product[label])
+                item_info.append(item[label])
             except KeyError:
-                prodcuct_info.append(None)
-    return prodcuct_info
+                item_info.append(None)
+    return item_info
 
 def get_collection_information(collection, labels):
     """Get array of information from collection, based on given list of labels"""
     collection_info = []
     for item in collection.find():
-        collection_info.append(get_product_information(item, labels))
+        collection_info.append(get_item_information(item, labels))
     return collection_info
 
 
