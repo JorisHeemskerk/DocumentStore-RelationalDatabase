182bdb0631456c36d7438e5f170d46afa71e321d
JorisHeemskerk <joris.heemskerk@student.hu.nl>
2022-03-14 13:02:12 +0100

    changed capitalisation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import MongoDB.session as ses
 import MongoDB.read_collection as read
-import PostgreSQL.Connect_db as connect
+import PostgreSQL.connect_db as connect
 
 collection = ses.get_collection(ses.get_database(ses.get_client(), "opisop"), "products")
 collection_data = read.get_collection_information(collection, ["_id", "name", ["price", "selling_price"]])
